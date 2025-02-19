9041bac89ec3632e4088dc1a55abbb4517768826
Egmarortiz <egmarocasio9@gmail.com>
2025-02-19 15:08:00 -0400

    fix

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Prints the uppercase and lowercase alphabet using only 3 putchar calls.
+ * main - Prints the uppercase and lowercase alphabet
  *
  * Return: 0 Always.
  */
 int main(void)
 {
-    int i;
-    int letter;
+int i;
+int letter;
 
-    for (i = 0; i < 52; i++)
-    {
-        if (i < 26)
-            letter = 'A' + i;
-        else
-            letter = 'a' + i - 26;
-        putchar(letter);  /* 1st putchar call in the source code */
-        if (i == 25)
-            putchar('\n');  /* 2nd putchar call in the source code */
-    }
-    putchar('\n');  /* 3rd putchar call in the source code */
+for (i = 0; i < 52; i++)
+{
+if (i < 26)
+letter = 'A' + i;
+else
+letter = 'a' + i - 26;
+putchar(letter);
+if (i == 25)
+putchar('\n');
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
