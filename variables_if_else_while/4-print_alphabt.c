808f20b79f6633870118dedfed2986d9d6ff391c
Egmarortiz <egmarocasio9@gmail.com>
2025-02-20 09:48:52 -0400

    fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -13,8 +13,8 @@ int main(void)
 char abc;
 {
 for (abc = 'a'; abc <= 'z'; abc++)
-	if (abc == 'q' || abc == 'e')
-		continue;
+if (abc == 'q' || abc == 'e')
+continue;
 putchar(abc);
 }
 putchar('\n');
