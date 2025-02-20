94152f48f75ab22af024a869879878c45f206873
Egmarortiz <egmarocasio9@gmail.com>
2025-02-20 10:58:38 -0400

    fix

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-        char abc;
+	char abc;
 
-        for (abc = 'z'; abc >= 'a'; abc--)
+	for (abc = 'z'; abc >= 'a'; abc--)
 	{
 		putchar(abc);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
