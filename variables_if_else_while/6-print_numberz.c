d6b6016bafb6fed1128586c8e9f8e972b1fea795
Egmarortiz <egmarocasio9@gmail.com>
2025-02-20 10:52:13 -0400

    fix

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: Progamm prints numbers
- * 
+ *
  * Return: Always 0 (Success)
  */
- 
- int main(void)
+
+int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num ++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
 	}
