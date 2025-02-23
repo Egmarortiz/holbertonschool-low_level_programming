3eaad20604c9c7b6676f1e5300d260b1307c1977
Egmarortiz <egmarocasio9@gmail.com>
2025-02-23 16:16:59 -0400

    fix

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *
+ * _isalpha - checks for character
+ *
+ * @c: checks for character
+ *
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+
+}
