1650c584bbf5eac36da383aa926056c453b0c875
Egmarortiz <egmarocasio9@gmail.com>
2025-02-23 15:42:08 -0400

    fix

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
