4fe50ff1ce227978c236e86eccc679be066f23dd
Egmarortiz <egmarocasio9@gmail.com>
2025-02-23 12:52:12 -0400

    fix

---
+++ functions_nested_loops/alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+}
