940c324079b9134488d5a1071e488f04a7ef2541
Egmarortiz <egmarocasio9@gmail.com>
2025-02-25 15:32:46 -0400

    fix

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_square - Function description
+ *
+ * @size: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int row, col; 
+
+	if (size <= 0)
+		_putchar('\n');
+	for(row = 0; row < size; row++)
+	{
+		for (col = 0; col < row; col++)
+			_putchar('#');
+	}
+
+	_putchar('#'); 
+	_putchar('\n');
+}
