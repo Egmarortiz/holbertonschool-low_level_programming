e4f5c921f176395b235897549fd7461028bbe53d
Egmarortiz <egmarocasio9@gmail.com>
2025-02-25 10:37:49 -0400

    fix

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isdigit  - Function description
+ *
+ * @c: Parameter description (if applicable)
+ *
+ * Return: Always 0 and 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (0);
+	else 
+		return (0);
+}
