d32ebb19ad28142e10c372de0d386aca760ba43d
Egmarortiz <egmarocasio9@gmail.com>
2025-02-25 16:03:34 -0400

    fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -14,14 +14,15 @@ void print_square(int size)
 	int row, col; 
 
 	if (size <= 0)
-		_putchar('\n');
-	for(row = 0; row < size; row++)
 	{
-		for (col = 0; col < row; col++)
+		_putchar('\n');
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
 		{
 			_putchar('#');
 		}
-		_putchar('#'); 
 		_putchar('\n');
 	}
 }
