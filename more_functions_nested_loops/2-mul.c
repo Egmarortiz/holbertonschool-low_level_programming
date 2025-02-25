e82da3ee7456872a1509ee8cc401a42b50138702
Egmarortiz <egmarocasio9@gmail.com>
2025-02-25 10:55:44 -0400

    fix

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * function_name - Function description
+ *
+ * @x: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+ 
+ int mul(int a, int b)
+{
+
+  return (a * b);
+
+}
