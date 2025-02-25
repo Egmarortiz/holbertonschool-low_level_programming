82ed93e7f074d78b9b94ef23eb0d4dce3ed81cfe
Egmarortiz <egmarocasio9@gmail.com>
2025-02-25 14:33:23 -0400

    fix

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -14,8 +14,9 @@ void print_line(int n)
 	int j;
 
 	if (n <= 0)
-		_putchar('\n');  
+		_putchar('\n'); 
 
-	for (j = 0; j > n; j++)
+	for (j = 0; j < n; j++)
 		_putchar('_');
+	_putchar('\n');
 }
