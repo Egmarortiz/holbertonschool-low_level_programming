4ee0c5e7b21009d22bb623828fc909c832bb62d4
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 10:13:42 -0400

    fix

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * function_name - Function description
+ *
+ * @a Parameter description (if applicable)
+ *
+ * @b: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+ 
+ void swap_int(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
