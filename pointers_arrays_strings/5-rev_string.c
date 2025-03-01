0ef843ef3656e056ab5b9f9fd936f92f715ea83f
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 12:11:42 -0400

    fix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,22 +7,22 @@
  */
 void rev_string(char *s)
 {
-    int i = 0, j = 0;
-    char temp;
+	int i = 0, j = 0;
+	char temp;
 
-    /* Find the length of the string */
-    while (s[j] != '\0')
-        j++;
-    j--;  /* Set j to the index of the last character */
+	/* Find the length of the string */
+	while (s[j] != '\0')
+		j++;
+	j--;  /* Set j to the index of the last character */
 
-    /* Swap characters from the beginning and the end */
-    while (i < j)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
-    }
+	/* Swap characters from the beginning and the end */
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
 }
 
