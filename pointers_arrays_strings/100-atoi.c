64283b6c61fc798ca56c3fda61aa221bf6095aee
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 13:02:06 -0400

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,37 +5,35 @@
  * _atoi - converts a string to an integer.
  * @s: the string to be converted.
  *
- * Description: The function processes all the '-' and '+' characters
- * preceding the number to determine its sign. Then it converts the
- * subsequent digits into an integer. If no digits are found, it returns 0.
+ * Description: The function scans through the string, processing all
+ * the '-' and '+' signs until it finds the first digit. It then converts
+ * the following digit characters into an integer. If no digits are found,
+ * the function returns 0.
  *
  * Return: the integer value represented by the string.
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int sign = 1;
+	int result = 0;
 
-	/* Process characters until a digit is encountered.
-	   Take into account all '+' and '-' signs before the number.
-	   */
-	while (s[i] != '\0')
+	/* Process all characters until we encounter a digit or end of string */
+	while (*s)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-			break;
-		if (s[i] == '-')
+		/* If the character is a '-' or '+', update the sign accordingly */
+		if (*s == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-		
-		i++;
+		else if (*s >= '0' && *s <= '9')
+			break;  /* Found the first digit; stop processing non-digit characters */
+		s++;
 	}
 
-	/* Convert digit characters into an integer */
-	while (s[i] >= '0' && s[i] <= '9')
+	/* Convert the digit characters into an integer */
+	while (*s && (*s >= '0' && *s <= '9'))
 	{
-		result = result * 10 + (s[i] - '0');
-		i++;
+		result = result * 10 + (*s - '0');
+		s++;
 	}
 
 	return (sign * result);
 }
-
