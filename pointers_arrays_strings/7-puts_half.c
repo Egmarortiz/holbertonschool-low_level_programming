bc076fa0f3a986b36fead2ccc0fa003761d58b02
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 12:27:56 -0400

    fix

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: The string to be printed.
+ *
+ * If the number of characters is odd, it prints the last n characters,
+ * where n = (length_of_the_string + 1) / 2.
+ */
+void puts_half(char *str)
+{
+	int len = 0, start, i;
+
+	/* Calculate the length of the string */
+	while (str[len] != '\0')
+		len++;
+
+	/* Determine the starting index for printing */
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = len - ((len + 1) / 2);
+
+	/* Print the second half of the string */
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
