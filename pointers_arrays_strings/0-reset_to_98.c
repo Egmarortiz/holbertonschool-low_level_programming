f22601f5e1230a2702c3b2d94fed7e9db8f26ca2
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 10:01:53 -0400

    fix

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * function_name - Function description
+ *
+ * @x: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
