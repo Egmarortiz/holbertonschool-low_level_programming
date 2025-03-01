bb0ab29dbfaee152a78da366313938996f2acb2e
Egmarortiz <egmarocasio9@gmail.com>
2025-03-01 10:15:13 -0400

    fix

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -10,10 +10,10 @@
  *
  * Return: Always 0
  */
- 
- void swap_int(int *a, int *b)
+
+void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
