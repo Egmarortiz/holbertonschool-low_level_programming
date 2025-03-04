1a3357f1c8cc18eee1a8d8452de4a9b1b8d5d0f7
Egmarortiz <egmarocasio9@gmail.com>
2025-03-04 13:15:10 -0400

    fix

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * *_strpbrk - Function description
+ *
+ * @s: Parameter description (if applicable)
+ * @accept: Parameter description (if applicable)
+ * @a: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *a = accept;
+
+		while (*a)
+		{
+			if (*s == *a)
+				return (char *)s;
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
