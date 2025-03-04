722dbd5be7867f7f8f22fcc72ec136629042c1cd
Egmarortiz <egmarocasio9@gmail.com>
2025-03-04 16:40:34 -0400

    fix

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * *_strstr - Function description
+ *
+ * @haystack: Parameter description (if applicable)
+ * @needle: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		return(haystack);
+
+	for(; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h != '\0' && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return(haystack);
+	}
+	return (NULL);
+}
