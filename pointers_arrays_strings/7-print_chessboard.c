30927ce3170706964d78ddc05e0d654c281110e8
Egmarortiz <egmarocasio9@gmail.com>
2025-03-05 09:03:50 -0400

    fix

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Function description
+ *
+ * @a: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+ 
+ void print_chessboard(char (*a)[8])
+{
+	
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf("%d ", a[i][j]);
+	}
+	printf("\n");
+}
