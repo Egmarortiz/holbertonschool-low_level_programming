8896cbd0cd24d7beb64ebd3f93531211abf4a087
Egmarortiz <egmarocasio9@gmail.com>
2025-03-07 10:14:47 -0400

    fix

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_puts_recursion - Function description
+ *
+ * @x: Parameter description (if applicable)
+ *
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
+
