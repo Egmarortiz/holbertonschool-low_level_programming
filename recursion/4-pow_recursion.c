22f073c725289cc65c45a316356cbe84114acc60
Egmarortiz <egmarocasio9@gmail.com>
2025-03-07 13:20:02 -0400

    fix

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,17 +2,18 @@
 #include "main.h"
 
 /**
- * function_name - Function description
+ * _pow_recursion - Function description
  *
  * @x: Parameter description (if applicable)
+ * @y: Parameter description (if applicable)
  *
  * Return: Always 0
  */
- 
- int _pow_recursion(int x, int y)
+
+int _pow_recursion(int x, int y)
 
 {
-    if (y < 0)
+	if (y < 0)
 		return (-1);
 	else if (y == 0)
 		return (1);
