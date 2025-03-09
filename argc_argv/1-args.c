69bcbbb22681f0259d993fadf7118a4501b0de5e
Egmarortiz <egmarocasio9@gmail.com>
2025-03-09 18:37:54 -0400

    fix

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed
+ * @argv: Array of arguments (not used in this program)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
