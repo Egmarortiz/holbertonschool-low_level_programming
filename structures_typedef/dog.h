c6ffee883761b2228e387b7e3a3512085ba4d8ab
Egmarortiz <egmarocasio9@gmail.com>
2025-03-16 20:16:02 -0400

    fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - structure that stores information about a dog
  * @name: the dog's name
@@ -12,4 +15,5 @@ typedef struct dog
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+#endif
 
