d93975cf22588eaa5c134269b8d9b1a61bd95fe7
Egmarortiz <egmarocasio9@gmail.com>
2025-03-16 20:12:11 -0400

    fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,8 @@
-/**
- * struct dog - structure that stores information about a dog
- * @name: the dog's name
- * @age: the dog's age
- * @owner: the dog's owner
- */
 typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+  char *name;
+  float age;
+  char *owner;
 } dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
