64bee4d95192cb47f466398103f8072e0f78cb35
Egmarortiz <egmarocasio9@gmail.com>
2025-03-16 20:47:01 -0400

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates a new dog.
@@ -13,6 +12,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *name_copy, *owner_copy;
+	unsigned int name_len = 0, owner_len = 0;
+	unsigned int i;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
@@ -20,27 +22,42 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d->age = age;
 
-	if (name)
+	/* Copy name */
+	if (name != NULL)
 	{
-		d->name = strdup(name);
-		if (d->name == NULL)
+		while (name[name_len])
+			name_len++;
+		name_copy = malloc(name_len + 1);
+		if (name_copy == NULL)
 		{
 			free(d);
 			return (NULL);
 		}
+		for (i = 0; i < name_len; i++)
+			name_copy[i] = name[i];
+		name_copy[name_len] = '\0';
+		d->name = name_copy;
 	}
 	else
 		d->name = NULL;
 
-	if (owner)
+	/* Copy owner */
+	if (owner != NULL)
 	{
-		d->owner = strdup(owner);
-		if (d->owner == NULL)
+		while (owner[owner_len])
+			owner_len++;
+		owner_copy = malloc(owner_len + 1);
+		if (owner_copy == NULL)
 		{
-			free(d->name);
+			if (d->name != NULL)
+				free(d->name);
 			free(d);
 			return (NULL);
 		}
+		for (i = 0; i < owner_len; i++)
+			owner_copy[i] = owner[i];
+		owner_copy[owner_len] = '\0';
+		d->owner = owner_copy;
 	}
 	else
 		d->owner = NULL;
